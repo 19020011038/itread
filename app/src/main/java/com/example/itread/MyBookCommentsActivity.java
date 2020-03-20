ade262b690f1123d9c53be1d3113d88fbd471dd4
qsh <2820108029@qq.com>
2020-03-20 21:09:25 +0800

    heiheihei

---
+++ app/src/main/java/com/example/itread/MyBookCommentsActivity.java
@@ -0,0 +1,14 @@
+package com.example.itread;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyBookCommentsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_my_book_comments);
+    }
+}
---
+++ app/src/main/java/com/example/itread/WriteBookCommentsActivity.java
@@ -0,0 +1,14 @@
+package com.example.itread;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WriteBookCommentsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_write_book_comments);
+    }
+}
