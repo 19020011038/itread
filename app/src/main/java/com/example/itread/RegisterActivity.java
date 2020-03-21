b6ccdb87714d34b0aabdfd429486b1787a98ec2e
zyr5509893798 <1327256380@qq.com>
2020-03-21 13:34:17 +0800

    初步完成注册UI,新加back图标，更改string文件，更改输入框文件

--- app/src/main/java/com/example/itread/RegisterActivity.java
+++ app/src/main/java/com/example/itread/RegisterActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private int a;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
