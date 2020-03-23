ca55e49f1a3e83ff2868de5e3343abcceeea6cb3
zyr5509893798 <1327256380@qq.com>
2020-03-23 21:09:31 +0800

    更改登录界面3月23日赵艳然

--- app/src/main/java/com/example/itread/LoginActivity.java
+++ app/src/main/java/com/example/itread/LoginActivity.java
@@ -2,13 +2,28 @@ package com.example.itread;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private RelativeLayout login_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_login);
+
+        login_register = findViewById(R.id.login_register);
+    }
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.login_register:
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
