f67f5f75b3f9fcffc488c90e440f1584de4b8db8
yueyuxuan-APP <1459987672@qq.com>
2020-04-11 21:22:51 +0800

    4.11  4.11

--- app/src/main/java/com/example/itread/Ui/fragment/guide/NewBookFragment.java
+++ app/src/main/java/com/example/itread/Ui/fragment/guide/NewBookFragment.java
@@ -59,33 +59,34 @@ public class NewBookFragment extends Fragment {
     private int k = 0;
     private boolean flag = false;
     private Handler net_fail;
+    private Handler book_finish;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        for(int i= 0;i<20;i++){
+        list.clear();
+        for (int i = 0; i < 20; i++) {
             Map map = new HashMap();
-            map.put("status",ab);
+            map.put("status", ab);
             list2.add(map);
             k++;
         }
         check = SharedPreferencesUtil.getInstance(getActivity());
         View root = inflater.inflate(R.layout.fragment_newbook, container, false);
-        recyclerView = (RecyclerView)root.findViewById(R.id.recyclerView5);
+        recyclerView = (RecyclerView) root.findViewById(R.id.recyclerView5);
         LinearLayoutManager manager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(manager);
         mAdapter = new NewBookAdapter(getActivity());
         recyclerView.setAdapter(mAdapter);
+
         //联网请求获得图书信息
-        if(k == 20){
-            mAdapter.setData2(list2);
+        if (k == 20) {
+            Log.i("联网获取新书","NewbookWithOkHttp");
+//            mAdapter.setData2(list2);
             NewbookWithOkHttp("http://47.102.46.161/AT_read/book_list/");
         }
-        Log.d("11111111111111111111",a);
-        Log.d("是否登录",String.valueOf(check.isLogin()));
 
-
-
+        Log.i("执行了 on create","123");
 
 
 //asdasdasdasdasdasdasdsadasdasdasdasd
@@ -96,17 +97,18 @@ public class NewBookFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        if (check.isLogin())
-        {
-            Log.d("2222222222222222222",a);
-            list2.clear();
-            NewBookStatus("http://47.102.46.161/AT_read/20/");
+        if(flag){
+            Log.i("执行了 on resume","123");
+            if(check.isLogin()){
+                list2.clear();
+                NewBookStatus2("http://47.102.46.161/AT_read/20/");
+            }
         }
-
-
+        flag = true;
     }
 
-    public void NewBookStatus(String address){
+
+    public void NewBookStatus(String address) {
         HttpUtil.NewBookStatus(address, new Callback() {
 
             @Override
@@ -117,11 +119,12 @@ public class NewBookFragment extends Fragment {
                 message.what = 1;
                 net_fail.sendMessage(message);
             }
+
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 //得到服务器返回的具体内容
                 final String responseData = response.body().string();
-                Log.d("caonimacaonimacaocaocao",responseData);
+                Log.d("caonimacaonimacaocaocao", responseData);
                 try {
                     n = 0;
 
@@ -135,7 +138,7 @@ public class NewBookFragment extends Fragment {
                         status = jsonObject1.getString("status");
 
                         Map map1 = new HashMap();
-                        map1.put("status",status);
+                        map1.put("status", status);
                         list2.add(map1);
                         n++;
 
@@ -145,9 +148,73 @@ public class NewBookFragment extends Fragment {
                         getActivity().runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                Log.d("4444444444",a);
-                                if(n == 20){
+
+                                if (n == 20) {
+                                    Log.i("在获取图书状态的结果中绑定适配器",String.valueOf(list));
+                                    recyclerView.setAdapter(mAdapter);
                                     mAdapter.setData(list);
+                                    mAdapter.setData2(list2);
+                                }
+                            }
+                        });
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        net_fail = new Handler(getActivity().getMainLooper()) {
+            public void handleMessage(Message message) {
+                super.handleMessage(message);
+                if (true) {
+                    Toast.makeText(getActivity(), "网络连接失败qwq\n请检查您的网络设置", Toast.LENGTH_LONG).show();
+                }
+            }
+        };
+    }
+
+    public void NewBookStatus2(String address) {
+        HttpUtil.NewBookStatus(address, new Callback() {
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                //在这里对异常情况进行处理
+                //       Toast.makeText(getActivity(),"获取图书信息失败，请检查您的网络",Toast.LENGTH_LONG).show();
+                Message message = new Message();
+                message.what = 1;
+                net_fail.sendMessage(message);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                //得到服务器返回的具体内容
+                final String responseData = response.body().string();
+                Log.d("caonimacaonimacaocaocao", responseData);
+                try {
+                    n = 0;
+
+                    JSONObject jsonObject = new JSONObject(responseData);
+                    JSONArray jsonArray = jsonObject.getJSONArray("book");
+
+                    for (int i = 0; i < jsonArray.length(); i++) {
+
+                        JSONObject jsonObject1 = jsonArray.getJSONObject(i);
+
+                        status = jsonObject1.getString("status");
+
+                        Map map1 = new HashMap();
+                        map1.put("status", status);
+                        list2.add(map1);
+                        n++;
+
+
+                    }
+                    if (!getActivity().equals(null))
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+
+                                if (n == 20) {
                                     mAdapter.setData2(list2);
                                     mAdapter.notifyDataSetChanged();
                                 }
@@ -163,14 +230,14 @@ public class NewBookFragment extends Fragment {
             public void handleMessage(Message message) {
                 super.handleMessage(message);
                 if (true) {
-                    Toast.makeText(getActivity(),"网络连接失败qwq\n请检查您的网络设置",Toast.LENGTH_LONG).show();
+                    Toast.makeText(getActivity(), "网络连接失败qwq\n请检查您的网络设置", Toast.LENGTH_LONG).show();
                 }
             }
         };
     }
 
     //获得图书信息的方法
-    public void NewbookWithOkHttp(String address){
+    public void NewbookWithOkHttp(String address) {
         HttpUtil.NewbookWithOkHttp(address, new Callback() {
 
             @Override
@@ -181,6 +248,7 @@ public class NewBookFragment extends Fragment {
                 message.what = 1;
                 net_fail.sendMessage(message);
             }
+
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 //得到服务器返回的具体内容
@@ -202,12 +270,12 @@ public class NewBookFragment extends Fragment {
                         book_id = jsonObject1.getString("num");
 
                         Map map = new HashMap();
-                        map.put("name",name);
-                        map.put("image",image);
-                        map.put("author",author);
-                        map.put("content",content);
-                        map.put("book_id",book_id);
-                        map.put("status",status);
+                        map.put("name", name);
+                        map.put("image", image);
+                        map.put("author", author);
+                        map.put("content", content);
+                        map.put("book_id", book_id);
+                        map.put("status", status);
 
                         list.add(map);
 
@@ -222,21 +290,36 @@ public class NewBookFragment extends Fragment {
                             public void run() {
 
 
-                                if (!getActivity().equals(null))
-                                {
-                                    if(!check.isLogin()){
-                                        Log.d("33333333333333333",a);
-                                        if(m == 20){
+                                if (!getActivity().equals(null)) {
+                                    if (!check.isLogin()) {
+
+                                        if (m == 20) {
+                                            Log.i("未登录时绑定适配器","recyclerView.setAdapter(mAdapter);");
+                                            recyclerView.setAdapter(mAdapter);
                                             mAdapter.setData(list);
                                             mAdapter.setData2(list2);
                                         }
-                                    }else {
-                                        list2.clear();
-                                        NewBookStatus("http://47.102.46.161/AT_read/20/");
+                                    } else {
+                                        if (m == 20) {
+                                            list2.clear();
+                                            Message message = new Message();
+                                            message.what = 1;
+                                            book_finish.sendMessage(message);
+                                        }
                                     }
                                 }
                             }
                         });
+                    book_finish = new Handler(getActivity().getMainLooper()) {
+                        public void handleMessage(Message message) {
+                            super.handleMessage(message);
+                            if (true) {
+                                Log.i("已经登录，联网获取图书状态"," NewBookStatus");
+                                NewBookStatus("http://47.102.46.161/AT_read/20/");
+
+                            }
+                        }
+                    };
 
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -247,7 +330,7 @@ public class NewBookFragment extends Fragment {
             public void handleMessage(Message message) {
                 super.handleMessage(message);
                 if (true) {
-                    Toast.makeText(getActivity(),"网络连接失败qwq\n请检查您的网络设置",Toast.LENGTH_LONG).show();
+                    Toast.makeText(getActivity(), "网络连接失败qwq\n请检查您的网络设置", Toast.LENGTH_LONG).show();
                 }
             }
         };
